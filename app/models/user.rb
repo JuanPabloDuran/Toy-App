07014b17141723594a09b8445ea6ff26b033697e
Juan Pablo <jpablodr11@gmail.com>
2021-01-12 16:52:01 -0600

    a little bit of documentation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
     has_many :microposts
     validates :name, presence: true
-    validates :email, presence: true
+    validates :email, presence: true #In the validation, before the ":" we need put the name of the field 
 end
